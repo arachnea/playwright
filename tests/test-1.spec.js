74cd3597723241c30436c1c8a1703370640ab9f5
Tony Fialho <33699153+arachnea@users.noreply.github.com>
2024-03-06 07:06:29 +0000

    changes

--- tests/test-1.spec.js
+++ tests/test-1.spec.js
@@ -1,15 +1,15 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
-test('test', async ({ page }) => {
-    await page.goto('https://www.testmo.com/');
+test('test main', async ({ page }) => {
+  await page.goto('https://www.testmo.com/');
 
   // Expect a title to contain the text.
   await expect(page.locator('h1')).toContainText('#1 Unified Modern TestManagement Software');
 });
 
-test('test', async ({ page }) => {
-    await page.goto('https://www.testmo.com/');
+test('test click', async ({ page }) => {
+  await page.goto('https://www.testmo.com/');
 
   // Click the See All Features link.
   await page.getByRole('link', { name: 'See All Features', exact: true }).click();
