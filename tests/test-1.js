c1c7785286d7d4ea630d9d8647d96b9316391bfd
Tony Fialho <33699153+arachnea@users.noreply.github.com>
2024-03-06 06:43:55 +0000

    test-1 creation

---
+++ tests/test-1.js
@@ -0,0 +1,19 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test('test', async ({ page }) => {
+    await page.goto('https://www.testmo.com/');
+
+  // Expect a title to contain the text.
+  await expect(page.locator('h1')).toContainText('#1 Unified Modern TestManagement Software');
+});
+
+test('test', async ({ page }) => {
+    await page.goto('https://www.testmo.com/');
+
+  // Click the See All Features link.
+  await page.getByRole('link', { name: 'See All Features', exact: true }).click();
+
+  // Expects page to have the features title.
+  await expect(page.locator('h1')).toContainText('Testmo Features Tour: Unified QA Testing');
+});
